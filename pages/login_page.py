f9629ef85ef3db94305f8a38a63bbc718e969534
Shila Shrestha <shilashrestha@Shilas-MacBook-Pro.local>
2025-12-07 23:36:54 +0545

    New change for login pahe

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,8 +8,10 @@ class LoginPage(BasePage):
 
     def open(self):
         self.navigate(Config.BASE_URL)
+    def go_to_login(self):
+        self.page.get_by_role("button", name="Login").click()
     
-    # def login(self, username: str, password: str):
-    #     self.page.fill(self.username_field, username)
-    #     self.page.fill(self.password_field, password)
-    #     self.page.click(self.login_button)
+    def login(self, username: str, password: str):
+        self.page.fill(self.username_field, username)
+        self.page.fill(self.password_field, password)
+        self.page.click(self.login_button)
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,27 @@
+# from pages.login_page import LoginPage
+
+# def test_login(page):
+#     login_page = LoginPage(page)
+#     login_page.open()
+#     login_page.login(Config.USERNAME, Config.PASSWORD)
+
+#     # assert page.url == "https://www.pogo.com/"
+#     # pass
+
+import os
 from pages.login_page import LoginPage
 
-def test_login(page):
+def test_should_login_successfully_with_valid_credentials(page):
+    email = os.getenv("EMAIL", "")
+    password = os.getenv("PASSWORD", "")
+    expected_logged_in_url = "/project/dashboard"
+
+    # Perform login
     login_page = LoginPage(page)
     login_page.open()
-    # assert page.url == "https://www.pogo.com/"
-    # pass
+    login_page.login(email, password)
+
+    # Validate redirection
+    assert expected_logged_in_url in page.url
+
+  
